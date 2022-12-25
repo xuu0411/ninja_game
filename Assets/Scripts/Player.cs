8116c1adc0c4e29579c9d4a816b4b03ec09ff2bc
xuu0411 <jie350182@gmail.com>
2022-12-25 21:35:55 +0800

    新增箭頭碰到貓咪音效

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public GameObject gameManager; //置放GameManager物件的公開變數
-
+    public AudioClip Walk;
+    public AudioClip Hurt;
     void Start()
     {
     }
@@ -15,6 +16,7 @@ public class Player : MonoBehaviour
         // 點擊左方向鈕時
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            GetComponent<AudioSource>().clip = Walk;
             GetComponent<AudioSource>().Play();
             transform.Translate(-3, 0, 0); // 往左移動「3」
         }
@@ -22,6 +24,7 @@ public class Player : MonoBehaviour
         // 點擊左方向鈕時
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            GetComponent<AudioSource>().clip = Walk;
             GetComponent<AudioSource>().Play();
             transform.Translate(3, 0, 0); // 往右移動「3」
         }
@@ -30,12 +33,15 @@ public class Player : MonoBehaviour
     // 當貓咪碰到其他有碰撞體的東西時
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        GetComponent<AudioSource>().clip = Hurt;
+        GetComponent<AudioSource>().Play();
         gameManager.GetComponent<GameManager>().DecreaseHp(); // 扣血
     }
 
     // 當玩家按下畫面左按鍵時，貓咪往左移動「3」
     public void LButtonDown()
     {
+        GetComponent<AudioSource>().clip = Walk;
         GetComponent<AudioSource>().Play();
         transform.Translate(-3, 0, 0);
     }
@@ -43,6 +49,7 @@ public class Player : MonoBehaviour
     // 當玩家按下畫面右按鍵時，貓咪往右移動「3」
     public void RButtonDown()
     {
+        GetComponent<AudioSource>().clip = Walk;
         GetComponent<AudioSource>().Play();
         transform.Translate(3, 0, 0);
     }
